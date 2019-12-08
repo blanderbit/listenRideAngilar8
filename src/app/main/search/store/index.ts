a4a2fd2db9e6120e96523d0f45004feafad325ce
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2019-12-08 14:57:03 +0200

    Added search state model to selectors

--- src/app/main/search/store/index.ts
+++ src/app/main/search/store/index.ts
@@ -1,5 +1,6 @@
 import {createFeatureSelector, createSelector} from "@ngrx/store";
+import SearchModel from "../search.types";
 
-export const getSearchState = createFeatureSelector('search');
+export const getSearchState = createFeatureSelector<SearchModel>('search');
 export const getBikes = createSelector(getSearchState, state => state.bikes);
-export const getLocations = createSelector(getSearchState, state => state.locations);
+export const getLocations = createSelector(getSearchState, state => state.location);
