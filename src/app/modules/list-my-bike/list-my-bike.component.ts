3367f9db79964033511d9e260e5c73117819b2dc
yarikroterdamskiygmailcom <yarik.roterdamskiy@gmail.com>
2020-02-13 15:47:56 +0200

    [LIST_MY_BIKE_V1.0]

--- src/app/modules/list-my-bike/list-my-bike.component.ts
+++ src/app/modules/list-my-bike/list-my-bike.component.ts
@@ -1,12 +1,12 @@
 import {
-    Component,
-    Inject,
-    OnInit
+  Component,
+  Inject,
+  OnInit
 } from '@angular/core';
 
 import {
-    select,
-    Store
+  select,
+  Store
 } from '@ngrx/store';
 
 import {
@@ -16,18 +16,18 @@ import {
 } from '@angular/forms';
 
 import {
-    typeList,
-    sizeList
+  typeList,
+  sizeList
 } from '@core/constants/filters.const';
 import {DOCUMENT} from '@angular/common';
 import * as fromAuth from '@auth/store/reducers';
 import {
-    AccessoriesImageInterface,
-    AccessoriesInterface,
-    CategoryInterface,
-    LoadedImageInterface,
-    SizeListInterface,
-    SubCategoryInterface
+  AccessoriesImageInterface,
+  AccessoriesInterface,
+  CategoryInterface,
+  LoadedImageInterface,
+  SizeListInterface,
+  SubCategoryInterface
 } from './model/models';
 import {DomSanitizer} from '@angular/platform-browser';
 
@@ -39,165 +39,165 @@ import {BIKE} from "@models/bike/bike.model";
 declare var require;
 
 @Component({
-    selector: 'lnr-list-my-bike',
-    templateUrl: './list-my-bike.component.html',
-    styleUrls: ['./list-my-bike.component.scss']
+  selector: 'lnr-list-my-bike',
+  templateUrl: './list-my-bike.component.html',
+  styleUrls: ['./list-my-bike.component.scss']
 })
 
 export class ListMyBikeComponent implements OnInit {
-    isLinear = false;
-    categoryFormGroup: FormGroup;
-    detailsFormGroup: FormGroup;
-    picturesFormGroup: FormGroup;
-    locationFormGroup: FormGroup;
-    pricingFormGroup: FormGroup;
-    bikeCategoryList: Array<CategoryInterface> = typeList;
-    sizeList: Array<SizeListInterface> = sizeList;
-    loadedPhoto: Array<LoadedImageInterface> = [];
-    subCategoriesValue: Array<SubCategoryInterface> | null = [];
-    user: Store<fromAuth.State> | any = this.store.pipe(select(fromAuth.selectAuthGetUser));
-    hide = true;
-    accessories: AccessoriesInterface | any = new AccessoriesInterface();
-    accessoriesImage: AccessoriesImageInterface | any = new AccessoriesImageInterface();
-    accessoriesArrList: Array<string> = [];
-    customisedPricing = false;
-    bikeQuantity = [0];
-    listPrices: Array<number> = [1000, 2000, 3000, 4000, 5000, 6000]
+  isLinear = false;
+  categoryFormGroup: FormGroup;
+  detailsFormGroup: FormGroup;
+  picturesFormGroup: FormGroup;
+  locationFormGroup: FormGroup;
+  pricingFormGroup: FormGroup;
+  bikeCategoryList: Array<CategoryInterface> = typeList;
+  sizeList: Array<SizeListInterface> = sizeList;
+  loadedPhoto: Array<LoadedImageInterface> = [];
+  subCategoriesValue: Array<SubCategoryInterface> | null = [];
+  user: Store<fromAuth.State> | any = this.store.pipe(select(fromAuth.selectAuthGetUser));
+  hide = true;
+  accessories: AccessoriesInterface | any = new AccessoriesInterface();
+  accessoriesImage: AccessoriesImageInterface | any = new AccessoriesImageInterface();
+  accessoriesArrList: Array<string> = [];
+  customisedPricing = false;
+  bikeQuantity = [0];
+  listPrices: Array<number> = [1000, 2000, 3000, 4000, 5000, 6000]
 
-    get accessoriesARrr() {
-        return (this.accessoriesArrList || []);
-    }
+  get accessoriesARrr() {
+    return (this.accessoriesArrList || []);
+  }
 
-    set accessoriesARrr(value) {
-        this.accessoriesArrList = Object.keys(value);
-    }
+  set accessoriesARrr(value) {
+    this.accessoriesArrList = Object.keys(value);
+  }
 
-    constructor(
-        private store: Store<fromAuth.State>,
-        private formBuilder: FormBuilder,
-        @Inject(DOCUMENT) private document: Document,
-        private matIconRegistry: MatIconRegistry,
-        private domSanitizer: DomSanitizer,
-        private apiRidesService: ApiRidesService
-    ) {
-        this.accessoriesARrr = this.accessories;
-        this.setSvgImageToMat();
-    }
+  constructor(
+    private store: Store<fromAuth.State>,
+    private formBuilder: FormBuilder,
+    @Inject(DOCUMENT) private document: Document,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+    private apiRidesService: ApiRidesService
+  ) {
+    this.accessoriesARrr = this.accessories;
+    this.setSvgImageToMat();
+  }
 
-    setSvgImageToMat(): void {
-        const images = require.context('../../../assets/img-accessories', true, /\.(png|jpg|jpeg|svg)$/);
-        images
-            .keys()
-            .forEach((key: string): void => {
-                const name = this.getClearName(key);
-                this.matIconRegistry
-                    .addSvgIcon(
-                        `lnr-${name}`,
-                        this.domSanitizer.bypassSecurityTrustResourceUrl(this.getUrlSvg(name))
-                    );
-            });
-    }
+  setSvgImageToMat(): void {
+    const images = require.context('../../../assets/img-accessories', true, /\.(png|jpg|jpeg|svg)$/);
+    images
+      .keys()
+      .forEach((key: string): void => {
+        const name = this.getClearName(key);
+        this.matIconRegistry
+          .addSvgIcon(
+            `lnr-${name}`,
+            this.domSanitizer.bypassSecurityTrustResourceUrl(this.getUrlSvg(name))
+          );
+      });
+  }
 
-    getClearName = (key: string): string => {
-        return key ? key
-                .replace('./', '')
-                .replace('.svg', '')
-            : '';
+  getClearName = (key: string): string => {
+    return key ? key
+        .replace('./', '')
+        .replace('.svg', '')
+      : '';
+  };
+
+  getUrlSvg = (name: string): string => name ? `../../../assets/img-accessories/${name}.svg` : '';
+
+  getUrlBackground = (value: any): string => value && value.src ? `url(${value.src}) 9px 3px no-repeat` : '';
+
+  ngOnInit(): void {
+
+    const category = {
+      category: ['', Validators.required],
+      subCategory: ['', Validators.required]
+    };
+    const detailsCtrl = {
+      available0: [true, Validators.required],
+      size0: ['', Validators.required],
+      frame_size0: ['', Validators.required],
+      bicycle_number0: ['', Validators.required],
+      frame_number0: ['', Validators.required],
+    };
+    const picturesCtrl = {
+      picturesCtrl_0: ['', Validators.required]
+    };
+    const locationCtrl = {
+      street: [''],
+      zip: [''],
+      city: [''],
+      country: [''],
+      custom_price: ['', Validators.required],
+    };
+    const pricingCtrl = {
+      daily: ['', Validators.required],
+      weekly: ['', Validators.required],
+      price: ['', Validators.required],
+      price1: ['', Validators.required],
+      price2: ['', Validators.required],
+      price3: ['', Validators.required],
+      price4: ['', Validators.required],
+      price5: ['', Validators.required],
+      price6: ['', Validators.required],
+      price7: ['', Validators.required],
     };
 
-    getUrlSvg = (name: string): string => name ? `../../../assets/img-accessories/${name}.svg` : '';
+    this.categoryFormGroup = this.formBuilder.group(category);
+    this.detailsFormGroup = this.formBuilder.group(detailsCtrl);
+    this.picturesFormGroup = this.formBuilder.group(picturesCtrl);
+    this.locationFormGroup = this.formBuilder.group(locationCtrl);
+    this.pricingFormGroup = this.formBuilder.group(pricingCtrl);
+  }
 
-    getUrlBackground = (value: any): string => value && value.src ? `url(${value.src}) 9px 3px no-repeat` : '';
+  changeCategory = (e: any): void => {
+    this.categoryFormGroup.controls.subCategory.setValue('');
+    this.subCategoriesValue = e.value.categories;
+  };
 
-    ngOnInit(): void {
+  previewFile(file: any): void {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      this.loadedPhoto.push({
+        isMain: false,
+        file,
+        url: reader.result
+      });
+    };
+  }
 
-        const category = {
-            category: ['', Validators.required],
-            subCategory: ['', Validators.required]
-        };
-        const detailsCtrl = {
-          available0: [true, Validators.required],
-          size0: ['', Validators.required],
-          frame_size0: ['', Validators.required],
-          bicycle_number0: ['', Validators.required],
-          frame_number0: ['', Validators.required],
-        };
-        const picturesCtrl = {
-            picturesCtrl_0: ['', Validators.required]
-        };
-        const locationCtrl = {
-          street: [''],
-          zip: [''],
-          city: [''],
-          country: [''],
-          custom_price: ['', Validators.required],
-        };
-        const pricingCtrl = {
-          daily: ['',Validators.required],
-          weekly: ['',Validators.required],
-          price: ['',Validators.required],
-          price1: ['',Validators.required],
-          price2: ['',Validators.required],
-          price3: ['',Validators.required],
-          price4: ['',Validators.required],
-          price5: ['',Validators.required],
-          price6: ['',Validators.required],
-          price7: ['',Validators.required],
-        } ;
+  removePhoto(i: number): void {
+    this.loadedPhoto.splice(i, 1);
+  }
 
-        this.categoryFormGroup = this.formBuilder.group(category);
-        this.detailsFormGroup = this.formBuilder.group(detailsCtrl);
-        this.picturesFormGroup = this.formBuilder.group(picturesCtrl);
-        this.locationFormGroup = this.formBuilder.group(locationCtrl);
-        this.pricingFormGroup = this.formBuilder.group(pricingCtrl);
-    }
+  create() {
+    const data = new BIKE();
+    this.apiRidesService.createBike(data);
+  }
 
-    changeCategory = (e: any): void => {
-        this.categoryFormGroup.controls.subCategory.setValue('');
-        this.subCategoriesValue = e.value.categories;
+  addVariants(index) {
+
+    const obj = {
+      size: ['', Validators.required],
+      frame_size: ['', Validators.required],
+      bicycle_number: ['', Validators.required],
+      frame_number: ['', Validators.required],
     };
 
-    previewFile(file: any): void {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onloadend = () => {
-            this.loadedPhoto.push({
-                isMain: false,
-                file,
-                url: reader.result
-            });
-        };
-    }
+    Object
+      .keys(obj)
+      .forEach(i => this.detailsFormGroup.addControl(`${i + (index)}`, new FormControl(...obj[i])));
 
-    removePhoto(i: number): void {
-        this.loadedPhoto.splice(i, 1);
-    }
+    const length = this.bikeQuantity.length;
+    this.bikeQuantity.push(length + 1);
+  }
 
-    create() {
-        const data = new BIKE();
-        this.apiRidesService.createBike(data);
-    }
-
-    addVariants(index) {
-
-        const obj = {
-          size: ['', Validators.required],
-          frame_size: ['', Validators.required],
-          bicycle_number: ['', Validators.required],
-          frame_number: ['', Validators.required],
-        };
-
-        Object
-            .keys(obj)
-            .forEach(i => this.detailsFormGroup.addControl(`${i + (index)}`, new FormControl(...obj[i])));
-
-        const length = this.bikeQuantity.length;
-        this.bikeQuantity.push(length + 1);
-    }
-
-    delQuantity(i) {
-        this.bikeQuantity.splice(i, 1);
-    }
+  delQuantity(i) {
+    this.bikeQuantity.splice(i, 1);
+  }
 
   category() {
     console.log(this.categoryFormGroup)
